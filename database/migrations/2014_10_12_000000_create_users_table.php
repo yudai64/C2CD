62dc4b9e33130b2bc9bf9afd4adf75819e737540
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-24 16:32:38 +0900

    ユーザーテーブル作成

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('user_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->char('postal_code', 8);
+            $table->string('address');
+            $table->char('phone_number', 13);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
