d8bbba9a4c390ac0214bca9f8c48145b0f3904a7
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-29 15:51:49 +0900

    商品テーブルのカラム名を修正

--- database/migrations/2020_06_29_051328_create_products__table.php
+++ database/migrations/2020_06_29_051328_create_products__table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('status_name');
+            $table->string('product_name');
             $table->string('image');
             $table->integer('price');
             $table->integer('amount');
