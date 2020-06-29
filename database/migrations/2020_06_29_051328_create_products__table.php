1bd5e19fab6c4e7ec3657b4cd96774e7abf39768
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-29 14:48:04 +0900

    商品テーブルを作成

---
+++ database/migrations/2020_06_29_051328_create_products__table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('status_name');
+            $table->string('image');
+            $table->integer('price');
+            $table->integer('amount');
+            $table->text('describe');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('status_id')->constrained()->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
