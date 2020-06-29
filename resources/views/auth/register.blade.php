175ec549357c549dc3a7fee2f7786377eebe67bc
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-29 17:45:23 +0900

    新規ユーザーメール登録画面の修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,7 +29,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="パスワードは8文字以上で入力してください">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
