8040097636756251005a7178a6a26f00cf20af47
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-30 15:24:04 +0900

    商品に関する機能に認証を追加

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('product/create');
