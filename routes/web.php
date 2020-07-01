0f4e29641efca69a7117f44a726a945f22e9aad5
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-07-01 12:05:37 +0900

    登録後の処理を修正(一時的)

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 
-// Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 
 
 // Auth::routes();
