576e1b7597814dc610eae29f0b52e0c4f8c7af48
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-07-28 19:26:43 +0900

    登録処理を修正

--- resources/views/product/confirm.blade.php
+++ resources/views/product/confirm.blade.php
@@ -33,6 +33,12 @@
                       {{ $post_data['describe'] }}
                     </div>
                     <div class="text-center">
+                    <input type="hidden" name="product_name" value="{{ $post_data['product_name'] }}">
+                    <input type="hidden" name="price" value="{{ $post_data['price'] }}">
+                    <input type="hidden" name="amount" value="{{ $post_data['amount'] }}">
+                    <input type="hidden" name="category_id" value="{{ $post_data['category_id'] }}">
+                    <input type="hidden" name="describe" value="{{ $post_data['describe'] }}">
+                    <input type="hidden" name="url" value="{{ $post_data['url'] }}">
                     <button type="submit" class="btn btn-primary ml-2 mt-3">{{ __('出品') }}</button>
                     <button type="button" class='btn btn-outline-dark ml-2 mt-3' onclick="history.back()">戻る</button>
                   </form>
