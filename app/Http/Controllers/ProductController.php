dadaebc208b119e7a7387442b3bb58f3ce54d882
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-07-28 15:52:33 +0900

    不要なコードを削除

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -46,53 +46,13 @@ class ProductController extends Controller
         
         $request->session()->put('post_data', $post_data);
         return view('product/confirm', compact('post_data'));
-
-
-        $image = $request->file('image');
-        $temp_path = $image->store('public/temp');
-        $read_temp_path = str_replace('public/', 'storage/', $temp_path);
-
-        $product_name = $request->product_name;
-        $price = $request->price;
-        $amount = $request->amount;
-        $describe = $request->describe;
-        $category_id = $request->category_id;
-        $category = DB::table('categories')->where('id', '=', $category_id)->value('category_name');
-        
-        $post_data = array(
-            'temp_path' => $temp_path,
-            'read_temp_path' => $read_temp_path,
-            'product_name' => $product_name,
-            'price' => $price,
-            'amount' => $amount,
-            'describe' => $describe,
-            'category_id' => $category_id,
-            'category' => $category,
-        );
-
-        $request->session()->put('post_data', $post_data);
-
-        return view('product/confirm', compact('post_data'));
     }
 
     public function store(Request $request, Product $product)
     {
         $post_data = session()->get('post_data');
-        // $temp_path = $post_data['temp_path'];
-        // $read_temp_path = $post_data['read_temp_path'];
-
-        // //ファイル名
-        // $filename = str_replace('public/temp/', '', $temp_path);
-        // //画像を保存するパス
-        // $storage_path = 'public/product/' . $filename;
         $read_path = $post_data['url'];
-
         $request->session()->forget('post_data');
-
-        //ファイルを移動
-        // Storage::move($temp_path, $storage_path);
-        // //画像を読み込むときのパス
-        // $read_path = str_replace('public/', 'storage/', $storage_path);
 
         $product->product_name = $post_data['product_name'];
         $product->price = (int)$post_data['price'];
